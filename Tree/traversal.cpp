2a8ab7926dfde3d596433160048ea83d3cd2a444
Pavankumar Mhaske <mhaskepavankumar@gmail.com>
2024-04-15 19:13:00 +0530

    chore: traversal is done

---
+++ Tree/traversal.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+class BinaryTree
+{
+    vector<int> nodes;
+    int idx;
+
+public:
+    BinaryTree(vector<int> &nodes) : nodes(nodes), idx(-1) {}
+
+    TreeNode *buildTree()
+    {
+        idx++;
+        if (nodes[idx] == -1)
+            return nullptr;
+
+        TreeNode *newNode = new TreeNode(nodes[idx]);
+
+        newNode->left = buildTree();
+        newNode->right = buildTree();
+
+        return newNode;
+    }
+};
+
+void preOrderTraversal(TreeNode *root)
+{
+    if (!root)
+        return;
+
+    cout << root->data << " ";
+    preOrderTraversal(root->left);
+    preOrderTraversal(root->right);
+}
+
+void inOrderTraversal(TreeNode *root)
+{
+    if (!root)
+        return;
+
+    inOrderTraversal(root->left);
+    cout << root->data << " ";
+    inOrderTraversal(root->right);
+}
+
+void postOrderTraversal(TreeNode *root)
+{
+    if (!root)
+        return;
+
+    postOrderTraversal(root->left);
+    postOrderTraversal(root->right);
+    cout << root->data << " ";
+}
+
+int main(void)
+{
+
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+    BinaryTree tree(nodes);
+
+    TreeNode *root = tree.buildTree();
+
+    cout << root->data << endl;
+
+    preOrderTraversal(root);
+    cout << endl;
+    inOrderTraversal(root);
+    cout << endl;
+    postOrderTraversal(root);
+    cout << endl;
+
+    return 0;
+}
