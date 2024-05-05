31852b9f879a96239f5f317848c67fe1c74b0451
P1kumarMhaske <mhaskepavankumar@gmail.com>
2024-05-05 14:38:28 +0530

    chore: finding the diameter

--- Trees/63BinaryTrees.cpp
+++ Trees/63BinaryTrees.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 // TreeNode - node
 class node
@@ -77,6 +78,52 @@ void levelOrder(node *root)
         }
     }
 }
+
+int height(node *root)
+{
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return max(leftHeight, rightHeight) + 1;
+}
+
+int diameter1(node *root) // O(n^2), O(height of T)
+{
+    if (root == NULL)
+        return 0;
+
+    int dia1 = diameter1(root->left);
+    int dia2 = diameter1(root->right);
+    int dia3 = height(root->left) + 1 + height(root->right);
+
+    return max(max(dia1, dia2), dia3);
+}
+
+pair<int, int> diameter(node *root) // O(n), O(height of T)
+{
+    if (root == NULL)
+        return make_pair(0, 0);
+
+    pair<int, int> left = diameter(root->left);
+    pair<int, int> right = diameter(root->right);
+
+    int dia1 = left.first;
+    int dia2 = right.first;
+    int dia3 = left.second + right.second + 1;
+
+    pair<int, int> info;
+    info.first = max(max(dia1, dia2), dia3);
+    info.second = max(left.second, right.second) + 1;
+    return info;
+}
+
+int findDiameter(node *root)
+{
+    return diameter(root).first;
+}
 // 1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
 
 int main(void)
@@ -85,6 +132,12 @@ int main(void)
     buildTreeFromLevelOrder(root);
     cout << endl;
     levelOrder(root);
+    cout << endl;
+    cout << "height of the tree is : " << height(root);
+    cout << endl;
+    // pair<int, int> info = diameter(root);
+    // cout << "Diameter of the tree is : " << info.first;
+    cout << "Diameter of the tree is : " << findDiameter(root);
 
     return 0;
 }
